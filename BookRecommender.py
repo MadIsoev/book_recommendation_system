3abaac75ac1bef1f4646cf372b0dc78e49be08d8
MadIsoev <isoeviso@gmail.com>
2025-05-08 13:19:27 +0500

    Update BookRecommender.py

--- BookRecommender.py
+++ BookRecommender.py
@@ -14,7 +14,8 @@ st.set_page_config(
 )
 
 # Новый стиль
-style>
+st.markdown("""
+    <style>
         @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Open+Sans:wght@400;600&display=swap');
 
         .main {
